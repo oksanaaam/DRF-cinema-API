ef667e148ff35d1ebd2123118c13174d6a3a3936
name <ksyusha0kubay@gmail.com>
2023-04-17 20:45:17 +0300

    Update README file

--- cinema_service/settings.py
+++ cinema_service/settings.py
@@ -10,11 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 import os
-from dotenv import load_dotenv
 from datetime import timedelta
 from pathlib import Path
-
-load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
